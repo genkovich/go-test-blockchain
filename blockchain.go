0c5048b01ace6f76cf4004e27d76b7f419b25aed
genkovich <genkovich@syneforge.com>
2023-07-19 23:16:37 +0300

    :boom: initial commit

---
+++ blockchain.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Blockchain struct {
+	genesisBlock Block
+	chain        []Block
+	difficulty   int
+}
+
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
+		timestamp: time.Now(),
+	}
+
+	return Blockchain{
+		genesisBlock: genesisBlock,
+		chain:        []Block{genesisBlock},
+		difficulty:   difficulty,
+	}
+}
+
+func (b *Blockchain) AddBlock(from, to string, amount float64) {
+	blockData := map[string]interface{}{
+		"from":   from,
+		"to":     to,
+		"amount": amount,
+	}
+
+	lastBlock := b.chain[len(b.chain)-1]
+	newBlock := Block{
+		data:         blockData,
+		previousHash: lastBlock.hash,
+		timestamp:    time.Now(),
+	}
+
+	newBlock.mine(b.difficulty)
+	b.chain = append(b.chain, newBlock)
+}
+
+func (b *Blockchain) IsValid() bool {
+	for i := range b.chain[1:] {
+		previousBlock := b.chain[i]
+		currentBlock := b.chain[i+1]
+		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previousHash != previousBlock.hash {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (b *Blockchain) String() string {
+	data, _ := json.MarshalIndent(b.chain, "", "  ")
+	return string(data)
+}
